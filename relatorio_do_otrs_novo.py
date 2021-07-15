a40c9161fb64a9bcd1abb6cd1d6ed56091f7a82c
rhuan-nasser <66943565+rhuan-nasser@users.noreply.github.com>
2021-07-15 17:26:38 -0300

    Update relatorio_do_otrs_novo.py

--- relatorio_do_otrs_novo.py
+++ relatorio_do_otrs_novo.py
@@ -3,7 +3,7 @@
 
 # # Bibliotecas
 
-# In[1]:
+# In[235]:
 
 
 import pandas as pd, datetime, time, os
@@ -15,7 +15,7 @@ from time import sleep as sl
 
 # # Variáveis
 
-# In[2]:
+# In[236]:
 
 
 mes = str(datetime.date.today().month)
@@ -28,19 +28,19 @@ minuto = completo[14:16]
 
 # # Chrome Driver
 
-# In[3]:
+# In[237]:
 
 
 web = webdriver.Chrome('/home/nasser/Documentos/chromedriver')
 web.get('http://urubu.in.iti.gov.br/otrs/index.pl')
-web.find_element_by_id('User').send_keys('<user>')
-web.find_element_by_id('Password').send_keys('<password>', Keys.ENTER)
+web.find_element_by_id('User').send_keys('<>')
+web.find_element_by_id('Password').send_keys('<>', Keys.ENTER)
 web.maximize_window()
 
 
 # # Acesso aos relatórios
 
-# In[4]:
+# In[238]:
 
 
 web.find_element_by_xpath('//*[@id="nav-Reports"]/a').click()
@@ -50,7 +50,7 @@ web.find_element_by_xpath('//*[@id="AppWrapper"]/div[3]/div[2]/div/div[2]/table/
 
 # # Altera a data de acordo com a data atual para a extração
 
-# In[5]:
+# In[239]:
 
 
 web.find_element_by_xpath(f'//*[@id="UseAsRestrictionCreateTimeStartMonth"]/option[{mes}]').click()
@@ -65,7 +65,7 @@ web.close()
 
 # # Renomeia o arquivo
 
-# In[6]:
+# In[240]:
 
 
 os.system(r'mv /home/nasser/Downloads/Relatório_*.xlsx /home/nasser/Downloads/relatorio.xlsx')
@@ -73,14 +73,12 @@ os.system(r'mv /home/nasser/Downloads/Relatório_*.xlsx /home/nasser/Downloads/r
 
 # # Tratamento dos dados
 
-# In[7]:
+# In[241]:
 
 
-#rel = pd.read_excel('/home/nasser/')
 rel = pd.read_excel('/home/nasser/Downloads/relatorio.xlsx')
 headers = rel.iloc[0]
 tabela = pd.DataFrame(rel.values[1:], columns=headers)
-#tabela = rel
 tabela['Tempo de solução em minutos'] = tabela['Tempo de solução em minutos'].astype(int)
 tabela['Primeira Resposta em Minutos'] = tabela['Primeira Resposta em Minutos'].astype(int)
 tabela['Delta de tempo de solução em minutos'] = tabela['Delta de tempo de solução em minutos'].astype(int)
@@ -88,7 +86,7 @@ tabela['Delta de tempo de solução em minutos'] = tabela['Delta de tempo de sol
 
 # # Quantidade de chamados estourados (Até 10 minutos)
 
-# In[60]:
+# In[242]:
 
 
 ate_10 = tabela[['Ticket#', 'Atendente/Proprietário', 'Primeira Resposta em Minutos', 'Criado', 'Primeira Resposta']].loc[tabela['Primeira Resposta em Minutos'] > 10]
@@ -102,10 +100,10 @@ else:
 # # Quantidade de chamados estourados (Até 1 hora)
 # 
 
-# In[59]:
+# In[243]:
 
 
-ate_1 = tabela[['Ticket#', 'Atendente/Proprietário', 'Tempo de solução em minutos', 'Criado', 'Primeira Resposta']].loc[(tabela['Delta de tempo de solução em minutos'] <= 0) & (tabela['Tempo de solução em minutos'] != 0)]
+ate_1 = tabela[['Ticket#', 'Atendente/Proprietário', 'Tempo de solução em minutos', 'Criado', 'Primeira Resposta', 'Delta de tempo de solução em minutos']].loc[(tabela['Delta de tempo de solução em minutos'] < 0) & (tabela['Tempo de solução em minutos'] != 0)]
 
 if not ate_1.empty:
     ate_1.to_excel('/home/nasser/Área de Trabalho/Relatório/chamados estourados de 1 hora.xlsx')
@@ -115,10 +113,10 @@ else:
 
 # # Quantidade de chamados estourados (Até 3 horas)
 
-# In[62]:
+# In[244]:
 
 
-ate_3 = tabela[['Ticket#', 'Atendente/Proprietário', 'Tempo de solução em minutos', 'Criado', 'Primeira Resposta']].loc[(tabela['Tempo de solução em minutos'] > 180) & (tabela['Delta de tempo de solução em minutos'] < 0)]
+ate_3 = tabela[['Ticket#', 'Atendente/Proprietário', 'Tempo de solução em minutos', 'Criado', 'Primeira Resposta', 'Delta de tempo de solução em minutos']].loc[tabela['Delta de tempo de solução em minutos'] < -120]
 
 if not ate_3.empty:
     ate_3.to_excel('/home/nasser/Área de Trabalho/Relatório/chamados estourados de 3 horas.xlsx')
@@ -128,27 +126,27 @@ else:
 
 # # Quantidade de chamados estourados (Até 36 horas)
 
-# In[61]:
+# In[245]:
 
 
-ate_36 = tabela[['Ticket#', 'Atendente/Proprietário', 'Tempo de solução em minutos', 'Criado', 'Primeira Resposta']].loc[(tabela['Tempo de solução em minutos'] > 2160) & (tabela['Delta de tempo de solução em minutos'] < 0)]
+ate_36 = tabela[['Ticket#', 'Atendente/Proprietário', 'Tempo de solução em minutos', 'Criado', 'Primeira Resposta', 'Delta de tempo de solução em minutos']].loc[tabela['Delta de tempo de solução em minutos'] < -2100]
 
 if not ate_36.empty:
-    ate_36.to_excel('/home/nasser/Área de Trabalho/Relatório/chamados estourados de 36 horas.xlsx')
+    ate_36.to_excel('/home/nasser/Área de Trabalho/Relatório/chamados estourados de 36 horas.xlsx', ascending=False)
 else:
     pass
 
 
 # # Agrupados por (Tipo)
 
-# In[66]:
+# In[246]:
 
 
 tipo = tabela[['Tipo','Ticket#']].groupby('Tipo').count().sort_values(by=['Ticket#'], ascending=False)
 tipo
 
 
-# In[72]:
+# In[247]:
 
 
 qtde_incidentes_requisicoes = list(tabela['Tipo'].value_counts())
@@ -166,7 +164,7 @@ plt.savefig('/home/nasser/Área de Trabalho/Relatório/Tickets por tipo.pdf', fo
 
 # # Agrupados por (Serviço)
 
-# In[74]:
+# In[248]:
 
 
 servico = tabela[['Serviço','Ticket#']].groupby('Serviço').count().sort_values(by=['Ticket#'], ascending=False)
@@ -175,7 +173,7 @@ servico.to_excel('/home/nasser/Área de Trabalho/Relatório/Por serviço.xlsx')
 
 # # Agrupados por (Setor)
 
-# In[71]:
+# In[249]:
 
 
 setor = tabela[['ID do Cliente','Ticket#']].groupby('ID do Cliente').count().sort_values(by=['Ticket#'], ascending=False)
@@ -184,7 +182,7 @@ setor.to_excel('/home/nasser/Área de Trabalho/Relatório/Por setor.xlsx')
 
 # # Agrupados por (Usuários)
 
-# In[76]:
+# In[250]:
 
 
 usuarios = tabela[['Usuário Cliente','Ticket#']].groupby('Usuário Cliente').count().sort_values(by=['Ticket#'], ascending=False)
@@ -193,7 +191,7 @@ usuarios.to_excel('/home/nasser/Área de Trabalho/Relatório/Por usuario.xlsx')
 
 # # Agrupados por (Estado)
 
-# In[77]:
+# In[251]:
 
 
 estado = tabela[['Estado', 'Ticket#']].groupby('Estado').count().sort_values(by=['Ticket#'], ascending=False)
@@ -202,7 +200,7 @@ estado.to_excel('/home/nasser/Área de Trabalho/Relatório/Por estado.xlsx')
 
 # # Coleta de informações para os gráficos
 
-# In[18]:
+# In[252]:
 
 
 correto_10 = tabela.loc[tabela['Primeira Resposta em Minutos'] <= 10]
@@ -211,36 +209,33 @@ nao_correto_10 = tabela.loc[tabela['Primeira Resposta em Minutos'] > 10]
 # Transforma a coluna Delta em str
 tabela['Delta de tempo de solução em minutos'] = tabela['Delta de tempo de solução em minutos'].astype(str)
 
-# Coleta todas as linhas que contém o delta negativo
-tabela['Delta de tempo de solução em minutos'] = tabela['Delta de tempo de solução em minutos'].str.replace(r'-', '')
-
 # Transforma a coluna Delta em int novamente
 tabela['Delta de tempo de solução em minutos'] = tabela['Delta de tempo de solução em minutos'].astype(int)
 
-correto_60 = tabela.loc[(tabela['Delta de tempo de solução em minutos'] <= 60) & (tabela['Tempo de solução em minutos'] != 0) ]
-nao_correto_60 = tabela.loc[tabela['Delta de tempo de solução em minutos'] > 60]
+correto_60 = tabela.loc[tabela['Delta de tempo de solução em minutos'] >= 0]
+nao_correto_60 = tabela.loc[tabela['Delta de tempo de solução em minutos'] < 0]
 
-correto_180 = tabela.loc[tabela['Delta de tempo de solução em minutos'] <= 120]
-nao_correto_180 = tabela.loc[tabela['Delta de tempo de solução em minutos'] > 120]
+correto_180 = tabela.loc[tabela['Delta de tempo de solução em minutos'] >= -120]
+nao_correto_180 = tabela.loc[tabela['Delta de tempo de solução em minutos'] < -120]
 
-correto_2160 = tabela.loc[tabela['Delta de tempo de solução em minutos'] <= 2100]
-nao_correto_2160 = tabela.loc[tabela['Delta de tempo de solução em minutos'] > 2100]
+correto_2160 = tabela.loc[tabela['Delta de tempo de solução em minutos'] > -2100]
+nao_correto_2160 = tabela.loc[tabela['Delta de tempo de solução em minutos'] <= -2100]
 
 
-c60 = correto_60['Ticket#'].count()
-nc60 = nao_correto_60['Ticket#'].count()
-c10 = correto_10['Ticket#'].count()
-nc10 = nao_correto_10['Ticket#'].count()
-c180 = correto_180['Ticket#'].count()
-nc180 = nao_correto_180['Ticket#'].count()
-c2160 = correto_2160['Ticket#'].count()
-nc2160 = nao_correto_2160['Ticket#'].count()
+c60 = correto_60.value_counts().sum()
+nc60 = nao_correto_60.value_counts().sum()
+c10 = correto_10.value_counts().sum()
+nc10 = nao_correto_10.value_counts().sum()
+c180 = correto_180.value_counts().sum()
+nc180 = nao_correto_180.value_counts().sum()
+c2160 = correto_2160.value_counts().sum()
+nc2160 = nao_correto_2160.value_counts().sum()
 qtde_incidentes_requisicoes = list(tabela['Tipo'].value_counts())
 
 
 # # Até 10 minutos
 
-# In[115]:
+# In[253]:
 
 
 fig = plt.figure(figsize=(10, 10))
@@ -257,7 +252,7 @@ plt.savefig('/home/nasser/Área de Trabalho/Relatório/Até 10 minutos', format=
 
 # # Até 60 minutos
 
-# In[114]:
+# In[254]:
 
 
 fig = plt.figure(figsize=(10, 10))
@@ -274,7 +269,7 @@ plt.savefig('/home/nasser/Área de Trabalho/Relatório/Até 1 hora', format='jpe
 
 # # Até 3 horas
 
-# In[113]:
+# In[255]:
 
 
 fig = plt.figure(figsize=(10, 10))
@@ -290,7 +285,7 @@ plt.savefig('/home/nasser/Área de Trabalho/Relatório/Até 3 horas', format='jp
 
 # # Até 36 horas
 
-# In[111]:
+# In[256]:
 
 
 fig = plt.figure(figsize=(10, 10))
@@ -305,7 +300,7 @@ plt.savefig('/home/nasser/Área de Trabalho/Relatório/Até 36 horas', format='j
 
 # # Por Serviço (Total):
 
-# In[23]:
+# In[257]:
 
 
 servico['Ticket#'].sum()
@@ -313,7 +308,7 @@ servico['Ticket#'].sum()
 
 # # TOP 10 Usuários
 
-# In[108]:
+# In[258]:
 
 
 top10 = usuarios.head(10).sort_values(by=['Ticket#'], ascending=True).plot.barh(width=0.7)
@@ -324,7 +319,7 @@ plt.savefig('/home/nasser/Área de Trabalho/Relatório/TOP 10', dpi = 300, bbox_
 # # Exclui a planilha do relatório
 # 
 
-# In[26]:
+# In[259]:
 
 
 os.system(r'rm -rf /home/nasser/Downloads/relatorio.xlsx')
