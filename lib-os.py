ff0f7dd9c2f9dfadc0273e7624ddf59e164431da
rhuan-nasser <rhuan_nasser@outlook.com>
2021-12-29 18:09:08 -0300

    Estudando lib os

---
+++ lib-os.py
@@ -0,0 +1,43 @@
+import os
+
+# Retorna informações sobre o sistema operacional, como: arquitetura, tipo de sistema operacional, nome do computador, versão do kernel e uptime.
+import stat
+
+print(f"SO: {os.uname()[0]}")
+print(f"Nome do Computador: {os.uname()[1]}")
+print(f"Versão do Kernel: {os.uname()[2]}")
+print(f"Versão do SO, hora e outros: {os.uname()[3]}")
+
+# retorna o terminal atual
+print(os.ctermid())
+
+# retorna as variáveis de ambiente
+print(os.environ)
+'''
+retorna o valor se for True a condição. Segue abaixo exemplo
+
+os.F_OK
+os.R_OK¶
+os.W_OK
+os.X_OK
+'''
+
+if os.access("/home/nasser/PycharmProjects/py/python/monitoramento_de_chamados_e_alerta_sonoro.py", os.W_OK):
+    with open("monitoramento_de_chamados_e_alerta_sonoro.py") as fp:
+        print(fp.read())
+
+# Acessa o diretório especificado
+os.chdir('/home/nasser')
+
+# transformar octal em decimal para o uso, o chmod faz a troca de permissão nas pastas e/ou arquivos
+os.chmod('/home/nasser/teste', 457)
+
+# executa um comando qualquer do Sistema operacional
+os.system('ls -l /home/nasser/teste')
+
+# troca o dono e o grupo do arquivo
+os.chown('/home/nasser/teste', 1000, 1000)
+os.system('ls -l /home/nasser/teste')
+
+# altera o diretório raiz
+os.chroot("/home/nasser")
--- linkedin.py
+++ linkedin.py
@@ -19,24 +19,52 @@ sl(2)
 nav.find_element(By.XPATH, '/html/body/div[3]/div/div/div[2]/ul/li[1]/fieldset/div/ul/li[1]/label/p/span[1]').click()
 sl(1)
 nav.find_element(By.XPATH, '/html/body/div[3]/div/div/div[3]/div/button[2]').click()
-
-
 sl(2)
-vagas = nav.find_elements(By.XPATH, '/html/body/div[5]/div[3]/div[3]/div[2]/div/section[1]/div/div/ul/li[*]')
+paginas = nav.find_elements(By.XPATH, "/html/body/div[5]/div[3]/div[3]/div[2]/div/section[1]/div/div/section/div/ul/li[*]/button")
 
-section1 = nav.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[3]/div[2]/div/section[1]')
 
-for vaga in vagas:
-    nav.execute_script("arguments[0].scrollBy(0, 500)", section1)
-    vaga.click()
-    descricao = nav.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[3]/div[2]/div/section[2]/div/div/div[1]/div/div[2]/article/div/div[*]')
-    print(f'''Vaga:
-
-{vaga.text}
-
-Descrição:          
-
-{descricao.text}
-          ''')
-    print('---------------------------------------------------------------------------')
+for pagina in paginas:
+    nav.execute_script('document.querySelector(".jobs-search-results").scrollBy(0,1000)')
     sl(2)
+    pagina.click()
+    sl(2)
+
+# while True:
+#
+#     for pagina in paginas:
+#
+#         for i in range(8):
+#             nav.execute_script('document.querySelector(".jobs-search-results").scrollBy(0,500)')
+#             sl(1)
+#
+#             if i == 7:
+#                 nav.execute_script('document.querySelector(".jobs-search-results").scrollTo(0,0)')
+#                 sl(2)
+#                 print(len(nav.find_elements(By.XPATH, '/html/body/div[5]/div[3]/div[3]/div[2]/div/section[1]/div/div/ul/li[*]/div/div/div[1]')))
+#
+#         num = int(pagina.text)
+#
+#         if num >= 2:
+#             nav.execute_script('document.querySelector(".jobs-search-results").scrollTo(0,0)')
+#             sl(2)
+#             print(pagina.text)
+#             pagina.click()
+#             sl(2)
+#             nav.execute_script('document.querySelector(".jobs-search-results").scrollBy(0,500)')
+
+# for n, vaga in enumerate(vagas):
+#     if (n%2) == 0 and n >= 4:
+#         nav.execute_script('document.querySelector(".jobs-search-results").scrollBy(0,1000)')
+#
+#     vaga.click()
+#     descricao = nav.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[3]/div[2]/div/section[2]/div/div/div[1]/div/div[2]/article/div/div[*]')
+#     print(f'''Vaga:
+#
+# {vaga.text}
+#
+# Descrição:
+#
+# {descricao.text}
+#           ''')
+#     print('---------------------------------------------------------------------------')
+#     sl(2)
