3e66b4cbe49b3180210da9d006a2c5c51a98cc6d
rhuan-nasser <rhuan_nasser@outlook.com>
2021-12-30 18:35:36 -0300

    Criando uma função e resumindo o código!

--- varrer_diretorios.py
+++ varrer_diretorios.py
@@ -6,6 +6,18 @@ caminho_de_destino_imagens = 'pasta_para_testes/imagens/'
 caminho_de_destino_arquivos = 'pasta_para_testes/arquivos/'
 caminho_de_destino_zipados = 'pasta_para_testes/zipados/'
 
+def verifica_tipo(dest):
+    if os.path.isdir(caminho + dest) == True:
+        src = caminho + file + f'/{item}'
+        dst = caminho + dest + item
+        shutil.copyfile(src, dst)
+    else:
+        os.mkdir(caminho + dest)
+        src = caminho + file + f'/{item}'
+        dst = caminho + dest + item
+        shutil.copyfile(src, dst)
+
+
 for file in os.listdir(caminho):
     if 'pasta_para_testes' in file:
         pass
@@ -16,36 +28,11 @@ for file in os.listdir(caminho):
 
             for item in lista:
                 if '.jpeg' in item or '.jpg' in item or '.png' in item:
-                    if os.path.isdir(caminho + caminho_de_destino_imagens) == True:
-                        src = caminho + file + f'/{item}'
-                        dst = caminho + caminho_de_destino_imagens + item
-                        shutil.copyfile(src, dst)
-                    else:
-                        os.mkdir(caminho + caminho_de_destino_imagens)
-                        src = caminho + file + f'/{item}'
-                        dst = caminho + caminho_de_destino_imagens + item
-                        shutil.copyfile(src, dst)
+                    verifica_tipo(caminho_de_destino_imagens)
 
                 if '.pdf' in item or '.doc' in item or '.docx' in item or '.xlsx' in item or '.odt' in item \
                         or '.html' in item or '.txt' in item:
-                    if os.path.isdir(caminho + caminho_de_destino_arquivos) == True:
-                        src = caminho + file + f'/{item}'
-                        dst = caminho + caminho_de_destino_arquivos + item
-                        shutil.copyfile(src, dst)
-                    else:
-                        os.mkdir(caminho + caminho_de_destino_arquivos)
-                        src = caminho + file + f'/{item}'
-                        dst = caminho + caminho_de_destino_arquivos + item
-                        shutil.copyfile(src, dst)
+                    verifica_tipo(caminho_de_destino_arquivos)
 
                 if '.zip' in item or '.7z' in item or '.xz' in item or '.bzip' in item or '.b2zip' in item:
-                    if os.path.isdir(caminho + caminho_de_destino_zipados) == True:
-                        src = caminho + file + f'/{item}'
-                        dst = caminho + caminho_de_destino_zipados + item
-                        shutil.copyfile(src, dst)
-
-                    else:
-                        os.mkdir(caminho + caminho_de_destino_zipados)
-                        src = caminho + file + f'/{item}'
-                        dst = caminho + caminho_de_destino_zipados + item
-                        shutil.copyfile(src, dst)
+                    verifica_tipo(caminho_de_destino_zipados)
